1188e95d9e39404a1059b0837ee5b539f219cad5
Kevin Dillane <dillane.k@gmail.com>
2025-02-20 19:27:39 -0800

    Fix static method

--- src/HeatPump.h
+++ src/HeatPump.h
@@ -304,6 +304,6 @@ class HeatPump
     // expert users only!
     void sendCustomPacket(byte data[], int len); 
 
-    const char* lookupRecvPacketName(const byte *packet);
+    static const char* lookupRecvPacketName(const byte *packet);
 };
 #endif
